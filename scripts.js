c1aca5da6b34a1b2ed05087bc4d45d6e28cfbaff
danboysk <williamsrka@gmail.com>
2024-02-06 18:55:07 +0000

    Starting files

---
+++ scripts.js
@@ -0,0 +1,14 @@
+// 1. Use the correct function name:
+fetch("https://api.openweathermap.org/data/2.5/weather?q=Leeds,uk&units=metric&APPID=f09dd0fbaa1b0be2f89ef1b93b05b4ff")
+  .then(response => response.json())
+  .then(data => {
+    console.log(data);
+  })
+  .catch(error => {
+    console.error("Error fetching weather data:", error);
+
+    // var icon = "https://api.openweathermap.org/img/w/" + data.weather[0].icon + ".png";
+    var weathericon = "https://api.openweathermap.org/img/w/" + data.weather[0].icon;
+
+    console.log(weathericon);
+  });
